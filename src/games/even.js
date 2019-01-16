3afcb6b470ee8cb5c9aa0345faf73ad49e09180c
Andrey Karamnoff <karamnoff@gmail.com>
2019-01-16 13:50:33 +0300

    use var for random number

--- src/games/even.js
+++ src/games/even.js
@@ -4,10 +4,11 @@ import { getRandomNumber, isEven } from '../helpers';
 import runGame from '../engine';
 
 const rules = 'Answer "yes" if number even otherwise answer "no".';
+const rand = 100;
 
 // get data for game's process
 const getData = () => {
-  const question = getRandomNumber(100);
+  const question = getRandomNumber(rand);
   const answer = isEven(question) ? 'yes' : 'no';
   const pair = cons(question, answer);
   return pair;
