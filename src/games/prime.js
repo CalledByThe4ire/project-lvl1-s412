6debde4f12ec40b861d65d03dcd57289087e0c73
Andrey Karamnoff <karamnoff@gmail.com>
2019-01-17 15:36:07 +0300

    add 'brain-prime' game

---
+++ src/games/prime.js
@@ -0,0 +1,31 @@
+import { cons } from 'hexlet-pairs';
+import getRandomNumber from '../helpers';
+import runGame from '../engine';
+
+const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+const randMin = 0;
+const randMax = 100;
+
+const isPrime = (number) => {
+  if (number < 2) {
+    return false;
+  }
+
+  for (let i = 2; i < number; i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+// get data for game's process
+const getData = () => {
+  const question = getRandomNumber(randMin, randMax);
+  const answer = isPrime(question) ? 'yes' : 'no';
+  const pair = cons(question, answer);
+  return pair;
+};
+
+export default () => runGame(getData, rules);
